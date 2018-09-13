6dc6a0ba69b1329af1ca18626608785c6396c50f
vikasbharadwaj <vikas.n.jayaram@gmail.com>
2018-09-13 19:48:06 +0530

    Add files via upload

---
+++ sketch1.js
@@ -0,0 +1,35 @@
+const classifier = ml5.imageClassifier('MobileNet', modelReady);
+
+// A variable to hold the image we want to classify
+let img;
+
+function setup() {
+  noCanvas();
+  // Load the image
+  img = createImg('', imageReady);
+  img.size(200,200);
+}
+
+// Change the status when the model loads.
+function modelReady(){
+  select('#status').html('Model Loaded')
+}
+
+// When the image has been loaded,
+// get a prediction for that image
+function imageReady() {
+  classifier.predict(img, gotResult);
+  // You can also specify the amount of classes you want
+  // classifier.predict(img, 10, gotResult);
+}
+
+// A function to run when we get any errors and the results
+function gotResult(err, results) {
+  // Display error in the console
+  if (err) {
+    console.error(err);
+  }
+  // The results are in an array ordered by probability.
+  select('#result').html(results[0].className);
+  select('#probability').html(nf(results[0].probability, 0, 2));
+}
